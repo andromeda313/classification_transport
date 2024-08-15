2df726c121a03931fb8efe56de244d1246fb6e78
andromeda313 <102717269+andromeda313@users.noreply.github.com>
2024-08-15 10:05:59 +0500

    adding
    ma'lumotlar yangilandi

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+
+import platform
+
+plt = platform.system()
+if plt=='Linux': 
+    pathlib.WindowsPath=pathlib.PosixPath
+
+st.title('Havo suv va yer transportlari klassifikatsiyasi')
+
+file = st.file_uploader('Rasmni yuklash', 
+                 type = ['jpg', 
+                         'gif', 
+                         'jpeg', 
+                         'svg'])
+
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    
+    model = load_learner('transport_model.pkl')
+    prediction, prob_id, probs = model.predict(img)
+    st.success(f'Bashorat: {prediction}')
+    st.info(f'Ehtimollik: {probs[prob_id]*100:.1f} %')
+    
+    fig = px.bar(x = probs*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
